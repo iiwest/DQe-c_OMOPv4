bda33f4b493c33a2e3117f457871b3fb8ee6ce8b
Ábtin Estiri <estiri.hossein@gmail.com>
2016-05-26 13:32:51 -0700

    Script with login information for ODBC connection.

---
+++ keys.R
@@ -0,0 +1,11 @@
+### This script is supposed to be confidential and only used by the person it is generated for.
+
+## what is your username for ODBC connection?
+usrnm <- "TYPE YOUR USERNAME HERE"
+
+## what is your password for ODBC connection?
+pss_OMOP <- "TYPE YOUR ODBC PASSWORD HERE"
+
+# who is running the test?
+tester <- "TYPE YOUR NAME HERE"
+
